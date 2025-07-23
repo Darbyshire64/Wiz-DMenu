6749c0d34ddcbf195f869dc8db5d68c8df0037d6
Lawri Darbyshire <darbyshire.lawri@gmail.com>
2025-07-24 00:04:41 +0100

    Added Sub Menu for brightness

--- light-rofi.sh
+++ light-rofi.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
 
-IP="10.252.0.214"   # Your bulb's IP
-PORT=38899          # UDP port for Wiz bulbs
+IP="10.252.0.214"   # Your Wiz bulb IP
+PORT=38899          # Wiz default port (UDP)
 
 send_udp_command() {
   local cmd=$1
   echo -n "$cmd" | nc -u -w1 "$IP" "$PORT"
 }
 
-OPTIONS="Turn On\nTurn Off\nCool White\nNeutral White\nWarm White"
+main_menu() {
+  echo -e "Turn On\nTurn Off\nCool White\nNeutral White\nWarm White\nBrightness" | \
+    rofi -dmenu -p "Wiz Light Control"
+}
 
-CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -p "Wiz Light Control")
+brightness_menu() {
+  echo -e "100%\n75%\n50%\n25%" | rofi -dmenu -p "Set Brightness"
+}
+
+handle_brightness() {
+  case "$1" in
+    "100%")
+      send_udp_command '{"method":"setPilot","params":{"dimming":100}}'
+      ;;
+    "75%")
+      send_udp_command '{"method":"setPilot","params":{"dimming":75}}'
+      ;;
+    "50%")
+      send_udp_command '{"method":"setPilot","params":{"dimming":50}}'
+      ;;
+    "25%")
+      send_udp_command '{"method":"setPilot","params":{"dimming":25}}'
+      ;;
+  esac
+}
+
+# --- Main logic ---
+CHOICE=$(main_menu)
 
 case "$CHOICE" in
   "Turn On")
@@ -20,16 +45,20 @@ case "$CHOICE" in
     send_udp_command '{"method":"setPilot","params":{"state":false}}'
     ;;
   "Cool White")
-    send_udp_command '{"method":"setPilot","params":{"temp":2700}}'
+    send_udp_command '{"method":"setPilot","params":{"temp":153}}'
     ;;
   "Neutral White")
-    send_udp_command '{"method":"setPilot","params":{"temp":4000}}'
+    send_udp_command '{"method":"setPilot","params":{"temp":320}}'
     ;;
   "Warm White")
-    send_udp_command '{"method":"setPilot","params":{"temp":6000}}'
+    send_udp_command '{"method":"setPilot","params":{"temp":454}}'
+    ;;
+  "Brightness")
+    BRI_CHOICE=$(brightness_menu)
+    handle_brightness "$BRI_CHOICE"
     ;;
   *)
-    # User cancelled or closed menu, do nothing
+    # Cancelled
     ;;
 esac
 
